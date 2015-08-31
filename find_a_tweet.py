66d6f28842f07fc06a15c569e45e67fdd7ab9476
jefk <speedracer111@gmail.com>
2015-08-31 15:37:27 -0700

    find a haiku and terminate

--- find_a_tweet.py
+++ find_a_tweet.py
@@ -11,11 +11,6 @@ from tweepy import Stream
 
 from haiku import Haiku
 
-access_token = os.environ['TWITTER_ACCESS_TOKEN']
-access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
-consumer_key = os.environ['TWITTER_CONSUMER_KEY']
-consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
-
 
 class HaikuListener(StreamListener):
 
@@ -23,28 +18,29 @@ class HaikuListener(StreamListener):
         tweet = json.loads(data)
         sys.stdout.flush()
 
-        if not (tweet.get('lang') and tweet['lang'] == 'en'):
-            print 'F',
-            return
-
         try:
             haiku = Haiku(tweet['text'])
             if haiku.is_valid():
                 print
                 print haiku.formatted()
-                sys.exit()
+                stream.disconnect()
 
-        except:
-            print 'E',
+        except Exception as e:
+            print e
             return
 
         print '.',
 
 
+access_token = os.environ['TWITTER_ACCESS_TOKEN']
+access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+stream = Stream(auth, HaikuListener())
+
+
 if __name__ == '__main__':
-
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, HaikuListener())
-
     stream.sample(languages=["en"])
