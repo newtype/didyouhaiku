d4b826c062e03276732954b6691251c1fe35d700
jefk <speedracer111@gmail.com>
2015-08-31 17:43:40 -0700

    rm debug

--- find_a_tweet.py
+++ find_a_tweet.py
@@ -16,20 +16,14 @@ class HaikuListener(StreamListener):
 
     def on_data(self, data):
         tweet = json.loads(data)
-        sys.stdout.flush()
 
         try:
             haiku = Haiku(tweet['text'])
             if haiku.is_valid():
-                print
                 print haiku.formatted()
                 stream.disconnect()
-
         except Exception as e:
-            print e
             return
-
-        print '.',
 
 
 access_token = os.environ['TWITTER_ACCESS_TOKEN']
